505060ce649456b7bdbbeaa97eed83b3ba8a407b
J. A. Rakib Mridha <239835216+jarmridha@users.noreply.github.com>
2026-05-06 08:07:06 +0600

    Add restored homepage using ProjectsSectionLite

---
+++ src/pages/IndexRestored.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import ProjectsSectionLite from "@/components/ProjectsSectionLite";
+import CertificationsSection from "@/components/CertificationsSection";
+import SkillsSection from "@/components/SkillsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const IndexRestored = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+    <AboutSection />
+    <ExperienceSection />
+    <ProjectsSectionLite />
+    <CertificationsSection />
+    <SkillsSection />
+    <ContactSection />
+    <Footer />
+  </>
+);
+
+export default IndexRestored;
